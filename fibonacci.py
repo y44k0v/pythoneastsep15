d05279a957cf735ab27dee1ee3f648861962a499
x11-test <xmr00011@gmail.com>
2025-09-21 03:47:30 -0400

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -7,7 +7,7 @@ def calculate_fibonacci(n):
     calculate_fibonacci(5) -> 5
     """
     # TODO: Student must write their code here.
-    # Formula solution
+    # Formula solution test
     if n < 0:
         raise ValueError("Sorry, numbers below zero are not allowed.")
     phi = (1 + 5**0.5) / 2
