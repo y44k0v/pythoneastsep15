7373cd3a7623f292a16c1355b7c682769862e4f8
DegraftMichael <degraftmichaelasare@gmail.com>
2025-09-20 22:29:22 -0400

    Modified the code to calculate the fibonacci number for 0,1 and 5

--- fibonacci.py
+++ fibonacci.py
@@ -8,4 +8,17 @@ def calculate_fibonacci(n):
     """
     # TODO: Student must write their code here.
     # For now, let's provide a placeholder that will fail the test.
-    return -1
+
+    """Return the nth Fibonacci number using recursion."""
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return calculate_fibonacci(n-1) + calculate_fibonacci(n-2)
+
+
+print("Fibonacci(0) =", calculate_fibonacci(0))
+print("Fibonacci(1) =", calculate_fibonacci(1))
+print("Fibonacci(5) =", calculate_fibonacci(5))
+ 
