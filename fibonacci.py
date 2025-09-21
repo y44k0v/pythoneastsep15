5c3719f659245b854b385126851497bf224d11db
y44k0v <17897299+y44k0v@users.noreply.github.com>
2025-09-21 15:38:51 -0400

    bmi test3

--- fibonacci.py
+++ fibonacci.py
@@ -6,6 +6,9 @@ def calculate_fibonacci(n):
     calculate_fibonacci(1) -> 1
     calculate_fibonacci(5) -> 5
     """
-    # TODO: Student must write their code here.
-    # For now, let's provide a placeholder that will fail the test.
-    return -1
+    if n < 0:
+        raise ValueError("Sorry, numbers below zero are not allowed.")
+    phi = (1 + 5**0.5) / 2
+    psi = (1 - 5**0.5) / 2
+
+    return round((phi**n - psi**n) / 5**0.5) 
