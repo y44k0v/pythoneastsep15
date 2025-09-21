6d688119780c5246d819052c7ea1ed4cbb8db5ca
x11-test <xmr00011@gmail.com>
2025-09-21 03:36:48 -0400

    Update fibonacci.py with formula

--- fibonacci.py
+++ fibonacci.py
@@ -7,5 +7,9 @@ def calculate_fibonacci(n):
     calculate_fibonacci(5) -> 5
     """
     # TODO: Student must write their code here.
-    # For now, let's provide a placeholder that will fail the test.
-    return -1
+    # Formula solution
+    if n < 0:
+        raise ValueError("Sorry, numbers below zero are not allowed.")
+    phi = (1 + 5**0.5) / 2
+    psi = (1 - 5**0.5) / 2
+    return round((phi**n - psi**n) / 5**0.5) 
