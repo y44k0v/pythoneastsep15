124dcfe8a49e5106bcbb64550005be4b990d82a9
Yaakov Yosef Miller <17897299+y44k0v@users.noreply.github.com>
2025-09-21 03:18:46 -0400

    corrected and commented fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -6,11 +6,13 @@ def calculate_fibonacci(n):
     calculate_fibonacci(1) -> 1
     calculate_fibonacci(5) -> 5
     """
-    # TODO: Student must write their code here.
-    # For now, let's provide a placeholder that will fail the test.
-
+    # Don't use recursion
+    
+    # Single lines are not triple quoted
     """Return the nth Fibonacci number using recursion."""
-    if n <= 0:
+    if n < 0:
+        raise ValueError("No negatives")
+    if n == 0:
         return 0
     elif n == 1:
         return 1
@@ -18,7 +20,7 @@ def calculate_fibonacci(n):
         return calculate_fibonacci(n-1) + calculate_fibonacci(n-2)
 
 
-print("Fibonacci(0) =", calculate_fibonacci(0))
-print("Fibonacci(1) =", calculate_fibonacci(1))
-print("Fibonacci(5) =", calculate_fibonacci(5))
+# print("Fibonacci(0) =", calculate_fibonacci(0))
+# print("Fibonacci(1) =", calculate_fibonacci(1))
+# print("Fibonacci(5) =", calculate_fibonacci(5))
  
