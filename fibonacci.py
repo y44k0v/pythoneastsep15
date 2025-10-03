255382ad1983f7964594cb8c228cbde591f97302
Vivekkumar Patel <vivekcanbe@gmail.com>
2025-10-03 14:36:39 -0300

    implemented fibonacci program

--- fibonacci.py
+++ fibonacci.py
@@ -6,6 +6,17 @@ def calculate_fibonacci(n):
     calculate_fibonacci(1) -> 1
     calculate_fibonacci(5) -> 5
     """
-    # TODO: Student must write their code here.
-    # For now, let's provide a placeholder that will fail the test.
-    return -1
+    if n<0:
+        raise ValueError("Negative Integers are not allowed")
+    
+    if n==0 : return 0
+    if n==1 : return 1
+
+    count=2
+    previous, current,next=0,1,0
+    while count<n+1:
+        next=current+previous
+        previous=current
+        current=next 
+        count+=1
+    return next
